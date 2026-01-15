cacade4ba95713b098ab59fc8f8ba0c69b4b2fab
aquaman122 <swj981128@gmail.com>
2026-01-15 11:07:20 +0900

    chore: 빌드 및 린트 오류 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/out/**",
+      "**/build/**",
+      "**/dist/**",
+      "**/*.config.js",
+      "**/*.config.mjs",
+      "**/*.config.ts",
+      "**/next-env.d.ts",
     ],
   },
 ];
-
-export default eslintConfig;
